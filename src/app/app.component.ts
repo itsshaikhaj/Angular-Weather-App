ffba72df3e3f858e36f0479501a366a2e2ffd5dc
itsshaikhaj <itsshaikhaj@gmail.com>
2023-04-29 23:54:56 +0530

    removed consoles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,31 +43,20 @@ export class AppComponent implements OnInit {
     subscribe(
       data2 => {
         this.countryInfo=data2.Countries;
-        console.log('this.countryInfo :', this.countryInfo);
-        //console.log('Data:', this.countryInfo);
       },
-      err => console.log(err),
-      () => console.log('complete')
     )
   }
 
   onChangeCountry(countryValue) {
-  console.log(' $$$$$$$$$$$:', );
-  console.log('countryValue :', countryValue);
     this.stateInfo=this.countryInfo[countryValue].States;
-    console.log('this.stateInfo :', this.stateInfo);
     this.cityInfo=this.stateInfo[0].Cities;
-    console.log(this.cityInfo);
   }
 
   onChangeState(stateValue) {
     this.cityInfo=this.stateInfo[stateValue].Cities;
-    console.log('this.cityInfo :', this.cityInfo);
-    //console.log(this.cityInfo);
   }
 
   onChangeCity(city) {
-  console.log('city :', city);
     const currentUrl = this.router.url;
     this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
         this.router.navigate([city]);
